5f3ba29e850608a6537507162e665ea63c22ffb5
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-04-05 19:47:56 +0430

    a buying list with edit btn +localStrorage saving

--- groceryBud-project/src/List.js
+++ groceryBud-project/src/List.js
@@ -1,7 +1,32 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-export default List
+const List = ({ list, removeItem, editItem }) => {
+  return (
+    <div className="grocery-list">
+      {list.map((item) => (
+        <article className="grocery-item" key={item.id}>
+          <p className="title">{item.title}</p>
+          <div className="btn-container">
+            <button
+              className="edit-btn"
+              type="button"
+              onClick={() => editItem(item.id)}
+            >
+              <FaEdit />
+            </button>
+            <button
+              className="delete-btn"
+              type="button"
+              onClick={() => removeItem(item.id)}
+            >
+              <FaTrash />
+            </button>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default List;
