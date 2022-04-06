448b11998a7df8f88573664d0ab45f921e11910e
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-04-06 20:04:51 +0430

    strarting

--- slider-modal-project/src/Sidebar.js
+++ slider-modal-project/src/Sidebar.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => (
+          <li key={link.id}>
+            <a href={link.url}>
+              {link.icon}
+              <span>{link.text}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-icons">
+        {social.map((social) => (
+          <li key={social.id}>
+            <a href={social.url}>{social.icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
