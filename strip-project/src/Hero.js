5f43d4dc938c424a9e611d8982e8efa3ae99ad8b
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-04-07 15:51:03 +0430

    dynamic submenu + dynamic subclasses

--- strip-project/src/Hero.js
+++ strip-project/src/Hero.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>payment infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt vel
+            veniam totam quidem voluptates dignissimos quod cupiditate error
+            voluptatem explicabo?
+          </p>
+          <button className="btn">start now</button>
+        </article>
 
-export default Hero
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
