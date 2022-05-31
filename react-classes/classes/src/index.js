49568e4bd6bf47ff788cbe37dd36c881621119b7
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-05-31 21:07:15 +0430

    ProcutsContext added and has acces to app but not up

--- react-classes/classes/src/index.js
+++ react-classes/classes/src/index.js
@@ -11,7 +11,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
