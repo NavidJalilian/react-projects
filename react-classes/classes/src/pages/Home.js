c20eec80f3a2a356479fa4d951f6747dec793c2d
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-06-06 23:50:55 +0430

    updation shop ui to grid

--- react-classes/classes/src/pages/Home.js
+++ react-classes/classes/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Card from "../components/Card";
-import {categories} from "../utils/Data";
-
+import { categories } from "../utils/Data";
+import "./Home.css";
 export default function Home() {
   return (
     <>
-      <div className="gallery">
+      <div className="home">
         {categories.map(({ id, title, imageUrl }) => (
           <Card url={imageUrl} title={title} key={id} />
         ))}
