45499dbb557bcec62325d43cc4f7d459e73bdd96
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-06-09 20:07:51 +0430

    filtering every category to show less than 4 in landing

--- react-classes/classes/src/pages/Shop.js
+++ react-classes/classes/src/pages/Shop.js
@@ -1,19 +1,17 @@
-import { Fragment, useContext } from "react";
+import {  useContext } from "react";
 import { CategoriesContext } from "../contexts/categoriesContext";
 import "./Shop.css";
-import { CartContext } from "../contexts/cartContext";
 import Category from "../components/Category";
 
 export default function Shope() {
   const { categoriesMap } = useContext(CategoriesContext);
 
-
-  return  <div className=""> 
-   { Object.keys(categoriesMap).map((title) => {
-    const item = categoriesMap[title];
-    return (<Category title={title} item={item} key={title} />) } </div>
-
-  
-      
-
-
+  return (
+    <div className="">
+      {Object.keys(categoriesMap).map((category) => {
+        const item = categoriesMap[category];
+        return <Category item={item} key={category} title={category} />;
+      })}
+    </div>
+  );
+}
