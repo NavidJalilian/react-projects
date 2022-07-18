5407a20555fa637d3e2898a61a90bc02b25750d9
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-18 20:32:31 +0430

    nabar to change hamburger menu on min pxs

--- eccommre-redxu/src/App.js
+++ eccommre-redxu/src/App.js
@@ -1,11 +1,14 @@
-import Card from "./Card";
-import Navbar from "./Navbar";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      <div className="gallery grid gap-2 place-items-center md:grid-cols-2  lg:grid-cols-3 sm:grid-cols-1">
+      <div
+        className="gallery pt-12 grid gap-2 place-items-center md:grid-cols-2 
+       lg:grid-cols-3  sm:grid-cols-2"
+      >
         <Card />
         <Card />
         <Card />
