602afd96bfb7f8e148341446e323cb23f4275718
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-18 15:44:22 +0430

    adding navbar ,gallery ui in app

--- eccommre-redxu/src/App.js
+++ eccommre-redxu/src/App.js
@@ -1,5 +1,18 @@
+import Card from "./Card";
+import Navbar from "./Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Navbar />
+      <div className="gallery grid gap-2 place-items-center md:grid-cols-2  lg:grid-cols-3 sm:grid-cols-1">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
