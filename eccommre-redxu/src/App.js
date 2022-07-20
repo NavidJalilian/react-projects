b48820c919a21bdf428e31775cefb2fadea16acc
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-20 13:06:08 +0430

    adding uaeState isloading indicator to shopLanding page =>needs to change to redux

--- eccommre-redxu/src/App.js
+++ eccommre-redxu/src/App.js
@@ -5,16 +5,20 @@ import Home from "./pages/Home";
 import CardDetails from "./pages/CardDetails";
 import ShopLanding from "./pages/ShopLanding";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setProcducts } from "./redux/actions/productAction";
 
 function App() {
-  const  {products}  = useSelector((state) => state.products);
+  const { products } = useSelector((state) => state.products);
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   const fetchProducts = async () => {
+    setIsLoading(true);
     const response = await axios
       .get("https://fakestoreapi.com/products")
       .catch((error) => console.log(error));
+    setIsLoading(false);
+
     dispatch(setProcducts(response.data));
   };
 
@@ -25,7 +29,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<ShopLanding products={products} />} />
+          <Route
+            index
+            element={<ShopLanding products={products} isLoading={isLoading} />}
+          />
           <Route path="/products/:id" element={<CardDetails />} />
         </Route>
       </Routes>
--- eccommre-redxu/src/pages/ShopLanding.js
+++ eccommre-redxu/src/pages/ShopLanding.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
+import SkeletonLoading from "../components/SkeletonLoading";
 
-export default function ShopLanding({ products }) {
+export default function ShopLanding({ products, isLoading }) {
+  console.log(isLoading);
+  if (isLoading) {
+    return <SkeletonLoading />;
+  }
   return (
     <div
       className="gallery pt-24 grid
